6089f823f62992ff67edb2435f93c74dd96bc905
Dimitar Dimitrov <me@ddimitrov.name>
2014-08-31 23:23:38 +0300

    Add validations to the Supporter model

--- app/models/supporter.rb
+++ app/models/supporter.rb
@@ -1,2 +1,5 @@
 class Supporter < ActiveRecord::Base
+  validates :email, uniqueness: true, presence: true
+  validates :first_name, :last_name, :supporter_kind, :sphere, presence: true
+  validates :email_confirmed, :approved, inclusion: [true, false]
 end
