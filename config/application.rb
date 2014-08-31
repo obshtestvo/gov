c673954a0a8bcae4e36b086bf7f3500d4197df54
Dimitar Dimitrov <me@ddimitrov.name>
2014-08-31 15:08:57 +0300

    Set the time zone to Sofia

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,7 @@ Bundler.require(*Rails.groups)
 
 module Gov
   class Application < Rails::Application
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Sofia'
     config.i18n.default_locale = :bg
   end
 end
