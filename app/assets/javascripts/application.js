fd829e471318d6a30d99b253ca6fe18492d23f20
Dimitar Dimitrov <me@ddimitrov.name>
2014-08-31 16:46:05 +0300

    Replace the dynamic SVG background with a solid color
    The solid color is just for now. We’ve abandoned D3 and Trianglify.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,27 +23,11 @@ $(function() {
   var $banner = $('.banner')
   var $header = $banner.find('header')
   var $nav = $('nav')
-  var $navPattern = $nav.find('.pattern')
-  var menubg = new Trianglify({
-      cellsize: 400,
-      noiseIntensity: 0,
-      x_gradient: ['#42c178', '#108eba', '#162e37']
-  });
-  var menupattern = menubg.generate($navPattern.width(), $navPattern.height());
-  $navPattern.css('background-image', menupattern.dataUrl)
 
-  var t = new Trianglify({
-    noiseIntensity: 0,
-    x_gradient: ['#42c178', '#108eba', '#162e37']
-  });
-
-  function redrawBanner() {
-    var minHeight = Math.max($win.height(), $header.height()*1.8)
-    $banner.css('min-height', minHeight+'px')
-    var pattern = t.generate($banner.width(), $banner.height());
-    $banner.css('background-image', pattern.dataUrl)
+  function resizeBanner() {
+    var minHeight = Math.max($win.height(), $header.height() * 1.8);
+    $banner.css('min-height', minHeight + 'px');
   };
-
 
   var menuOffset = 150;
 
@@ -76,13 +60,9 @@ $(function() {
     })
   })
 
-  $win.on('resize', function() {
-    redrawBanner();
-  });
-  redrawBanner()
-
+  $win.resize(resizeBanner);
+  resizeBanner();
   $nav.waypoint('sticky');
-
 
   var $openSourceBrowserAlert = $('.open-browser');
   var $browserName = $openSourceBrowserAlert.find('.browser')
