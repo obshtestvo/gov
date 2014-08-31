4193448d8da36cd1f3f749e8e6445b29db8071ee
Dimitar Dimitrov <me@ddimitrov.name>
2014-08-31 20:37:56 +0300

    Allow overriding the production server name via ENV
    This is useful when the deploying machine must use a different address
    than the publicly available one (e.g. when we’re deploying from the
    container onto itself).

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,6 @@
-server 'koi.obshtestvo.bg:2206', user: 'www-data', roles: %w(app web db)
+server  ENV['GOV_PRODUCTION_SERVER'] || 'koi.obshtestvo.bg:2206',
+        user: 'www-data',
+        roles: %w(app web db)
 
 set :deploy_to,       '/var/www/gov.obshtestvo.bg'
 set :puma_threads,    [15, 15]
