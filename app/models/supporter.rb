ccc39cd767f0b7704a5b28b7830d3197ee3d782a
jsibelius <c_c@abv.bg>
2014-09-06 14:14:20 +0300

    Added conditional validation for presence of organisation name.

--- app/models/supporter.rb
+++ app/models/supporter.rb
@@ -2,4 +2,5 @@ class Supporter < ActiveRecord::Base
   validates :email, uniqueness: true, presence: true
   validates :first_name, :last_name, :area, presence: true
   validates :email_confirmed, :approved, inclusion: [true, false]
+  validates :organization, presence: true, if: :is_organization?
 end
