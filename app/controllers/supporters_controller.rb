08ac27f7c51297a0de3ef523a775213322d0867f
jsibelius <c_c@abv.bg>
2014-09-08 21:07:02 +0300

    Added business logic addressing 'invalid token' and 'already verified email' situations.

--- app/controllers/supporters_controller.rb
+++ app/controllers/supporters_controller.rb
@@ -21,15 +21,25 @@ class SupportersController < ApplicationController
 
   def verify
     verification_code = params[:code]
-    skb = Rails.application.secrets.secret_key_base
-    verifier = ActiveSupport::MessageVerifier.new(skb)
-    id = verifier.verify(verification_code)
+    secret_key_base = Rails.application.secrets.secret_key_base
+    verifier = ActiveSupport::MessageVerifier.new(secret_key_base)
+
+    begin
+      id = verifier.verify(verification_code)
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+      flash[:alert] = "Невалиден персонален код."
+      redirect_to root_path
+    end
 
     s = Supporter.find(id)
-    s.email_confirmed = true
-    s.save
 
-    flash[:notice] = "Благодарим Ви, че потвърдихте вашият имейл. Вие вече подкрепяте инициативата."
+    if s.email_confirmed?
+      flash[:notice] = "Вие вече подкрепихте инициативата."
+    else
+      flash[:notice] = "Благодарим Ви, че потвърдихте вашият имейл. Вие вече подкрепяте инициативата."
+      s.email_confirmed = true
+      s.save
+    end
 
     redirect_to root_path
   end
