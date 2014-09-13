085fed78fc54c12032c525e209911036a2a6aee7
jsibelius <c_c@abv.bg>
2014-09-13 20:26:39 +0300

    Made people the default page and moved area buttons.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   resources :supporters
   get '/verify/:code', to: 'supporters#verify', as: :verify_supporter
-  get ':area' => 'home#index', constraints: { area: /administration|people|it/ }, as: :home
-  root 'home#index', :defaults => { :area => "administration" }
+  get ':area' => 'home#index', constraints: { area: /people|administration|it/ }, as: :home
+  root 'home#index', :defaults => { :area => "people" }
 end
