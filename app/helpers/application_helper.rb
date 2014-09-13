4361075227a7dfa2bda118d3206f13f6a4510ba5
Dimitar Dimitrov <me@ddimitrov.name>
2014-09-13 21:03:19 +0200

    Implement tooltip-based explanations of terms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def definition_of(term, *args)
+    options = args.extract_options!
+    options[:title] ||= t("definitions.#{term}").strip
+    args << options
+
+    content_tag :abbr, *args
+  end
 end
