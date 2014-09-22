ec2cf91e6f5063058a8865d58d077555d77e9214
Dimitar Dimitrov <me@ddimitrov.name>
2014-09-22 16:45:38 +0300

    Another stab at ajaxifying the view point tabs
    See bfefc5bb9b for more info. The second try is because some but not all
    JavaScript needs to be re-run when the page gets replaced.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,74 +20,6 @@
 //= require_tree .
 
 $(function() {
-  var $nav = $('nav')
-  var $fixedNav = $nav.clone()
-  var $header = $('.banner header');
-
-  var makeBannerHeaderFulHeight = function () {
-    var height = $(window).height();
-    var width  = $(window).width();
-    if (width > height * 1.2) {
-      $header.height(height);
-    } else {
-      $header.height('auto');
-    }
-  };
-  // $(window).resize(makeBannerHeaderFulHeight);
-  // makeBannerHeaderFulHeight();
-
-  $fixedNav.removeClass('main').addClass('fixed').insertAfter($nav);
-
-  var menuOffset = 150;
-
-  $fixedNav.find('a').each(function() {
-    var $a       = $(this);
-    var $item    = $a.parent();
-    var selector = $a.attr('href');
-    var $section = $(selector);
-
-    $a.on('click', function () {
-      setTimeout(function () {
-        $fixedNav.removeClass('active');
-      }, 300);
-    });
-
-    $section.waypoint(function(dir) {
-      $item.toggleClass('active', dir === 'up').next().toggleClass('active', false);
-    }, {
-      offset: function() {
-        return -$(this).height() + menuOffset;
-      }
-    })
-
-    $section.waypoint(function(dir) {
-      $item.toggleClass('active', dir === 'down').prev().toggleClass('active', false);
-    }, {
-      offset: menuOffset
-    })
-  });
-
-  $nav.waypoint(function(dir) {
-    if (dir=='up') {
-      $fixedNav.removeClass('on')
-    }
-  }, {
-    offset: function() {
-      return -$(this).height();
-    }
-  }).waypoint(function(dir) {
-    if (dir=='down') {
-      $fixedNav.addClass('on')
-    }
-  }, {
-    offset: function() {
-      return -$(this).height();
-    }
-  });
-  $fixedNav.find('.handle').click(function() {
-    $fixedNav.toggleClass('active')
-  })
-
   // Show a loader overlay over ajaxified forms
   $(document).on('ajax:send', 'form', function () {
     $(this).find('.loading').show();
@@ -131,27 +63,100 @@ $(function() {
   };
   $(document).on('change', '#supporter_is_organization', function () { updateSignFormState('animate') });
   $(document).on('ajax:updated', '#sign-form', function () { updateSignFormState(); });
-  updateSignFormState();
 
-  // Custom tooltips plugin
-  $('[title]').tipsy();
-  $('body > *').on('click', function () {
-    // This is an empty handler to allow click events on Mobile Safari. More info:
-    // http://www.quirksmode.org/blog/archives/2014/02/mouse_event_bub.html
-  });
+  var initializePage = function () {
+    var $nav = $('nav')
+    var $fixedNav = $nav.clone()
+    var $header = $('.banner header');
 
-  var $openSourceBrowserAlert = $('.open-browser');
-  var $browserName = $openSourceBrowserAlert.find('.browser');
-  if (bowser.firefox || bowser.chrome) {
-    $openSourceBrowserAlert.removeClass('hidden');
-    $browserName.text($browserName.data('firefox'));
-    if (bowser.firefox) {
-      $openSourceBrowserAlert.find('.firefox').removeClass('hidden');
+    var makeBannerHeaderFulHeight = function () {
+      var height = $(window).height();
+      var width  = $(window).width();
+      if (width > height * 1.2) {
+        $header.height(height);
+      } else {
+        $header.height('auto');
+      }
+    };
+    // $(window).resize(makeBannerHeaderFulHeight);
+    // makeBannerHeaderFulHeight();
+
+    $fixedNav.removeClass('main').addClass('fixed').insertAfter($nav);
+
+    var menuOffset = 150;
+
+    $fixedNav.find('a').each(function() {
+      var $a       = $(this);
+      var $item    = $a.parent();
+      var selector = $a.attr('href');
+      var $section = $(selector);
+
+      $a.on('click', function () {
+        setTimeout(function () {
+          $fixedNav.removeClass('active');
+        }, 300);
+      });
+
+      $section.waypoint(function(dir) {
+        $item.toggleClass('active', dir === 'up').next().toggleClass('active', false);
+      }, {
+        offset: function() {
+          return -$(this).height() + menuOffset;
+        }
+      })
+
+      $section.waypoint(function(dir) {
+        $item.toggleClass('active', dir === 'down').prev().toggleClass('active', false);
+      }, {
+        offset: menuOffset
+      })
+    });
+
+    $nav.waypoint(function(dir) {
+      if (dir=='up') {
+        $fixedNav.removeClass('on')
+      }
+    }, {
+      offset: function() {
+        return -$(this).height();
+      }
+    }).waypoint(function(dir) {
+      if (dir=='down') {
+        $fixedNav.addClass('on')
+      }
+    }, {
+      offset: function() {
+        return -$(this).height();
+      }
+    });
+    $fixedNav.find('.handle').click(function() {
+      $fixedNav.toggleClass('active')
+    })
+
+    updateSignFormState();
+
+    // Custom tooltips plugin
+    $('[title]').tipsy();
+    $('body > *').on('click', function () {
+      // This is an empty handler to allow click events on Mobile Safari. More info:
+      // http://www.quirksmode.org/blog/archives/2014/02/mouse_event_bub.html
+    });
+
+    var $openSourceBrowserAlert = $('.open-browser');
+    var $browserName = $openSourceBrowserAlert.find('.browser');
+    if (bowser.firefox || bowser.chrome) {
+      $openSourceBrowserAlert.removeClass('hidden');
+      $browserName.text($browserName.data('firefox'));
+      if (bowser.firefox) {
+        $openSourceBrowserAlert.find('.firefox').removeClass('hidden');
+      }
+      if (bowser.chrome) {
+        $openSourceBrowserAlert.find('.chrome').removeClass('hidden');
+      }
     }
-    if (bowser.chrome) {
-      $openSourceBrowserAlert.find('.chrome').removeClass('hidden');
-    }
-  }
+  };
+
+  initializePage();
 
   $(document).on('click', '.area-links a.button', function (e) {
     e.preventDefault();
@@ -162,6 +167,7 @@ $(function() {
       if (history.pushState) {
         history.replaceState({}, null, url);
       }
+      initializePage();
     });
     return false;
   });
